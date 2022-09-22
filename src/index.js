4399f488ce1452d70bd9c5c3662fc929c4cbb06e
Nguyenhoangphuong <khangnguyen8196@gmail.com>
2022-09-22 20:23:04 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ const app = express();
 const port = 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 
 // Template engine
@@ -19,15 +22,26 @@ app.set('views', path.join(__dirname, "resources","views"));
 // method
 app.get('/home', (req, res) => {
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body.q)
+  res.send('');
+});
+
+
+app.get('/search', (req, res) => {
+    // console.log(req.query.q)
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
    
 
